2c2770ac4bf4433dfcf8691d49796666cd8432d4
abdul moiez <155585384+abdulmoiez1506@users.noreply.github.com>
2024-01-22 07:46:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var length = prompt('Length of rectangle')
+var width = prompt('Width of rectangle')
+var area = length * width;
+console.log("The area of the rectangle is " + area); 
+
+
+
+var username = prompt("Whats your name?");
+alert("Hi, " + username + "! have a nice day." );
